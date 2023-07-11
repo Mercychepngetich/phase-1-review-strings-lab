313df3261d066f5a6cc7839bee204b839561fd3f
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-07-11 10:33:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const shortGreeting ="Welcome, G, !" ;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
